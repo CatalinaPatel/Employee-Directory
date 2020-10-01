141fb628ed8bd1f90a6f3e1c82f18a95f805fc31
Catalina Patel <patelcatalina@gmail.com>
2020-10-01 19:51:07 -0400

    Updated app.js & index.js

--- public/src/App.js
+++ public/src/App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <Home />
+        <Navbar />
+        <Wrapper />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={Home} />
+      </Wrapper>
+      <Footer />
     </div>
+    </Router >
   );
 }
 
